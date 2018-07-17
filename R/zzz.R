08b918d71b132d310b34c0a20a7c91d78e44c0aa
Russell V. Lenth <russell-lenth@uiowa.edu>
2018-07-16 20:41:59 -0500

    Tiny factual corrections

--- R/zzz.R
+++ R/zzz.R
@@ -46,9 +46,9 @@ hasName = function(x, name)
 
 
 ### Dynamic registration of S3 methods
-# Code borrowed from zoo pkg. I omitted some type checks etc. because
+# Code borrowed from hms pkg. I omitted some type checks etc. because
 # this is only for internal use and I solemnly promise to behave myself.
-register_s3_method <- function(pkg, generic, class) {
+register_s3_method = function(pkg, generic, class) {
     fun = get(paste0(generic, ".", class), envir = parent.frame())
     if (isNamespaceLoaded(pkg)) {
         registerS3method(generic, class, fun, envir = asNamespace(pkg))
