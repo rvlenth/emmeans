968384f922e988f34fb48fae96a8a6e40172c61d
Russell V. Lenth <russell-lenth@uiowa.edu>
2019-10-12 16:14:41 -0500

    Cleaned-up eff_size()
    also beefed up examples/documentation for emtrends()

--- R/eff-size.R
+++ R/eff-size.R
@@ -7,44 +7,38 @@
 #' divided by the SD of the population providing the context for those effects.
 #' This function calculates effect sizes from an \code{emmGrid} object,
 #' and confidence intervals for them, accounting for uncertainty in both the estimated
-#' effects and the population SD. There is some flexibility in specifying the
-#' needed parameters.
+#' effects and the population SD. 
 #' 
 #' Any \code{by} variables specified in \code{object} will remain in force in the returned
 #' effects, unless overridden in the optional arguments.
 #' 
-#' The \code{LM} parameter is recommended \emph{only} for models having a single
-#' random effect, such as those fitted using \code{\link{lm}}. For more complex
-#' models such as mixed models, the user usually should specify \code{SD} and
-#' one of the \code{SE} parameters manually, based on some combination of the
-#' random-effect variances.
+#' For models having a single random effect, such as those fitted using
+#' \code{\link{lm}}; in that case, the \code{stats::sigma} and
+#' \code{stats::df.residual} functions may be useful for spcifying \code{sigma}
+#' and \code{edf}. For models with more than one random effect, \code{sigma} may
+#' be based on some combination of the random-effect variances. 
 #' 
-#' It may be difficult to provide one of the \code{SE} parameters. But here are two 
-#' useful things to know: (1) If \code{SD^2} is proportional to a chi-square random variable,
-#' then \code{SE.logSD} equals \code{1 / sqrt(2 * df)}.
-#' (2) \code{200 * SE.logSD} is roughly equal to the percentage range for most variations
-#' in \code{SD}; for example, if you think \code{SD} is accurate to within 20 percent
-#' of its true value, then \code{SE.logSD = 0.1} is about right (and from the first point, 
-#' this corresponds to 50 degrees of freedom).
-#' 
-#' Specifying one of the \code{SE} parameters equal to zero is allowable, in which case you
-#' are assuming that \code{SD} is known exactly. Obviously, this narrows the confidence
-#' intervals for the effect sizes -- unrealistically if in fact \code{SD} is estimated.
-#' 
+#' Specifying \code{edf} can be rather unintuitive but is also relatively
+#' uncritical; but the smaller the value, the wider the confidence intervals for
+#' effect size. The value of \code{sqrt(2/edf)} can be interpreted as the
+#' relative accuracy of \code{sigma}; for example, with \code{edf = 50},
+#' \eqn{\sqrt(2/50) = 0.2}, meaning that \code{sigma} is accurate to plus or
+#' minus 20 percent. Note in an example below, we tried two different \code{edf}
+#' values as kind of a bracketing/sensitivity-analysis strategy. A value of
+#' \code{Inf} is allowable, in which case you are assuming that \code{sigma} is
+#' known exactly. Obviously, this narrows the confidence intervals for the
+#' effect sizes -- unrealistically if in fact \code{sigma} is unknown.
 #' 
 #'
 #' @param object an \code{\link[=emmGrid-class]{emmGrid}} object, typically one defining the EMMs to 
 #' be contrasted.
-#' @param SD numeric scalar, value of the population SD. 
-#'   This may be missing only if \code{LM} is specified.
-#' @param SE.logSD,SE.SD,SE.var numeric scalars that specify the standard
-#'   deviation of the sampling distribution of various functions of the
-#'   \code{SD} estimate -- respectively, that of \code{log(SD)}, \code{SD}
-#'   itself, or the variance, \code{SD^2}. The first non-missing value, in that
-#'   order, is the one that is used. All three of these may be missing if
-#'   \code{LM} is specified.
-#' @param LM a model object having \code{\link{sigma}} and
-#'   \code{\link{df.residual}} methods. See Details for important considerations.
+#' @param sigma numeric scalar, value of the population SD. 
+#' @param edf numeric scalar that specifies the equivalent degrees of freedom
+#'   for the \code{sigma}. This is a way of specifying the uncertainty in \code{sigma},
+#'   in that we regard our estimate of \code{sigma^2} as being proportional to
+#'   a chi-square random variable with \code{edf} degrees of freedom. (\code{edf} should
+#'   not be confused with the \code{df} argument that may be passed via \code{...}
+#'   to specify the degrees of freedom to use in \eqn{t} statistics and confidence intervals.)
 #' @param method the contrast method to use to define the effects.
 #'   This is passed to \code{\link{contrast}} after the elements of \code{object}
 #'   are scaled.
@@ -54,10 +48,10 @@
 #' @section Computation:
 #' This function uses calls to \code{\link{regrid}} to put the estimated
 #' marginal means (EMMs) on the log scale. Then an extra element is added to
-#' this grid for the log of \code{SD} and is standard error (where we assume
-#' that \code{SD} is uncorrelated with the log EMMs). Then a call to
-#' \code{\link{contrast}} subtracts \code{log{SD}} from each of the log EMMs,
-#' yielding values of \code{log(EMM/SD)}.
+#' this grid for the log of \code{sigma} and its standard error (where we assume
+#' that \code{sigma} is uncorrelated with the log EMMs). Then a call to
+#' \code{\link{contrast}} subtracts \code{log{sigma}} from each of the log EMMs,
+#' yielding values of \code{log(EMM/sigma)}.
 #' Finally, the results are re-gridded back to the original scale and the
 #' desired contrasts are computed using \code{method}. In the log-scaling
 #' part, we actually rescale the absolute values and keep track of the signs.
@@ -67,47 +61,55 @@
 #' any response transformation or link function is completely ignored. If you
 #' wish to base the effect sizes on the response scale, it is \emph{not} enough
 #' to replace \code{object} with \code{regrid(object)}, because this
-#' back-transformation changes the SD required to compute effect sizes. This is
-#' a can of worms not worth opening, in my opinion.
+#' back-transformation changes the SD required to compute effect sizes. 
+#' 
+#' @note
+#' \strong{Disclaimer:} There is substantial disagreement among practitioners on
+#' what is the appropriate \code{sigma} to use in computing effect sizes; or,
+#' indeed, whether \emph{any} effect-size measure is appropriate for some
+#' situations. The user is completely responsible for specifying 
+#' appropriate parameters (or for failing to do so).
 #' 
 #' @export
 #'
 #' @examples
 #' fiber.lm <- lm(strength ~ diameter + machine, data = fiber)
+#' 
 #' emm <- emmeans(fiber.lm, "machine")
-#' eff_size(emm, LM = fiber.lm)
+#' eff_size(emm, sigma = sigma(fiber.lm), edf = df.residual(fiber.lm))
 #' 
 #' # or equivalently:
-#' eff_size(pairs(emm), LM = fiber.lm, method = "identity")
+#' eff_size(pairs(emm), sigma(fiber.lm), df.residual(fiber.lm), method = "identity")
 #' 
-#' # compare with naively scaling by error SD without accounting for its uncertainty:
-#' confint(pairs(emm, scale = 1/sigma(fiber.lm), adjust = "none"))
-#' # This is more efficient than eff_size(emm, LM = fiber.lm, SE.logSD = 0)
+#' 
+#' ### Mixed model example:
+#' if (require(nlme)) {
+#' 
+#'   Oats.lme <- lme(yield ~ Variety + factor(nitro), 
+#'                   random = ~ 1 | Block / Variety,
+#'                   data = Oats)
+#'                   
+#'   # Combine variance estimates
+#'   VarCorr(Oats.lme)
+#'   totSD <- sqrt(214.4724 + 109.6931 + 162.5590)
+#'   # I figure edf is somewhere between 5 (Blocks df) and 51 (Resid df)
+#'   
+#'   emmV <- emmeans(Oats.lme, ~ Variety)
+#'   print(eff_size(emmV, sigma = totSD, edf = 5))
+#'   print(eff_size(emmV, sigma = totSD, edf = 51))
+#' }
+#' 
+#' # Multivariate model for the same data:
+#'  MOats.lm <- lm(yield ~ Variety, data = MOats)
+#'  eff_size(emmeans(MOats.lm, "Variety"), 
+#'           sigma = sqrt(mean(sigma(MOats.lm)^2)),   # RMS of sigma()
+#'           edf = df.residual(MOats.lm))
 #' 
 #' # These results illustrate a sobering message that effect sizes are often
 #' # not nearly as accurate as you may think.
-eff_size = function(object, SD, SE.logSD, SE.SD, SE.var, LM, 
-                    method = "pairwise", ...) {
-    # resolve parameter specs
-    if (!missing(LM)) {
-        if(missing(SD))  
-            SD = sigma(LM)
-        if(missing(SE.var))
-            SE.var = SD^2 * sqrt(2 / df.residual(LM))
-    }
-    if (missing(SD))
-        stop("Must specify 'SD' or 'LM'")
-    if (missing(SE.logSD)) {
-        if(!missing(SE.SD))
-            SE.logSD = SE.SD / SD
-        else if(!missing(SE.var))
-            SE.logSD = SE.var / SD^2 / 2
-        else {
-            message("Naive effect sizes: SEs are understated, CIs are too narrow")
-            SE.logSD = 0
-        }
-    }
-    
+eff_size = function(object, sigma, edf, method = "pairwise", ...) {
+    SE.logsigma = sqrt(1 / (2 * edf))
+ 
     object = update(object, tran = NULL)
     object = regrid(object, transform = "response")
     
@@ -121,10 +123,10 @@ eff_size = function(object, SD, SE.logSD, SE.SD, SE.var, LM,
     
     # put on log scale and incorporate the SD estimate
     logobj = regrid(object, tran = "log")
-    logobj@bhat = c(logobj@bhat, log(SD))
+    logobj@bhat = c(logobj@bhat, log(sigma))
     V = rbind(cbind(logobj@V, 0), 0)
     n = nrow(V)
-    V[n,n] = SE.logSD^2
+    V[n,n] = SE.logsigma^2
     logobj@V = V
     logobj@levels = list(dummy = 1:n)
     logobj@grid = data.frame(dummy = 1:n)
@@ -137,5 +139,5 @@ eff_size = function(object, SD, SE.logSD, SE.SD, SE.var, LM,
     object@V = con@V
     update(contrast(object, method, adjust = "none", ...), 
            estName = "effect.size", infer = c(TRUE, FALSE),
-           initMesg = paste("SD for effect sizes:", signif(SD, digits = 4)))
+           initMesg = paste("sigma used for effect sizes:", signif(sigma, digits = 4)))
 }
