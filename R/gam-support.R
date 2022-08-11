2575fcafc6a6edb4609eee092341f6aab384d9bf
Russell V. Lenth <russell-lenth@uiowa.edu>
2022-08-11 11:33:48 -0500

    Yet, yet, yet one more try (#365,#366,#369)

--- R/gam-support.R
+++ R/gam-support.R
@@ -113,17 +113,20 @@ emm_basis.Gam = function(object, trms, xlev, grid, nboot = 800, ...) {
          dfargs = dfargs, misc = misc)
 }
 
+# Local utility for identifying random smooths
+.smooth.is.random = function(s) 
+    (inherits(s, "random.effect") || inherits(s, "fs.interaction"))
 
 ### for mgcv::gam objects
 ### Many thanks to Maarten Jung for help on sorting-out fixed and random effects
 recover_data.gam = function(object, ...) {
     if (length(object$smooth) > 0) { # get rid of random terms
         fixnm = unlist(lapply(object$smooth, function(s) {
-                              if(inherits(s, "random.effect")) NA
+                              if(.smooth.is.random(s)) ""
                               else c(s$term, s$by)
             }))
         fixnm = union(.all.vars(delete.response(object$pterms)), fixnm)
-        fixnm = setdiff(fixnm, c("1", "NA"))
+        fixnm = setdiff(fixnm, c("1", "", "NA"))
         object$terms = terms(.reformulate(fixnm, env = environment(terms(object))))
     }
     recover_data.lm(object, ...)
@@ -137,7 +140,7 @@ emm_basis.gam = function(object, trms, xlev, grid,
                          what = c("location", "scale", "shape", "rate", "prob.gt.0"), 
                          ...) {
     if (length(object$smooth) > 0) { # get rid of random terms 
-        rand = sapply(object$smooth, function(s) {ifelse(inherits(s, "random.effect"), s$label, NA)})
+        rand = sapply(object$smooth, function(s) {ifelse(.smooth.is.random(s), s$label, NA)})
         rand = if (all(is.na(rand))) NULL else rand[!is.na(rand)]
     }
     else
