b5228d7f74287597170c37f5ebf1ecf3ca686038
jpiaskowski <julia.piask@gmail.com>
2025-12-10 15:15:35 -0800

    add cheatsheet

---
+++ maintainer-notes/cheatsheet.R
@@ -0,0 +1,9 @@
+
+# to load all functions into an R session
+devtools::load_all()
+
+# to rebuild documentation:
+roxygen2::roxygenise()
+
+# to run all unit tests
+testthat::test_dir("tests/testthat")
