ce84bb82c1014575235c51d914c9adc221aa6a62
Russell V. Lenth <russell-lenth@uiowa.edu>
2026-03-24 15:23:53 -0500

    Mods to mvregrid and its examples

--- inst/extexamples/mvregrid.R
+++ inst/extexamples/mvregrid.R
@@ -14,3 +14,17 @@ mvregrid(AVRG, newname = "comp", newlevs = lvls) |>
 
 # The ref grid we'd have gotten if the 'clr' transform had been used...
 mvregrid(AVRG, transform = "clr", newname = "comp", newlevs = lvls)
+
+### Use of fcn argument...
+arr.lm <- lm(as.matrix(USArrests) ~ state.region)
+arr.emm <- emmeans(arr.lm, ~ crime | state.region, mult.name = "crime")
+
+PC <- princomp(USArrests, cor = TRUE)
+std.and.rot <- function(x, pc, ...) {
+    z <- sweep(sweep(x, 2, pc$center, "-"), 2, pc$scale, "/")
+    z %*% pc$loadings
+}
+
+# EMMs obtained if model had been fitted with PC$scores as the response ...
+mvregrid(arr.emm, newname = "prin.comp", fcn = std.and.rot, pc = PC)
+
